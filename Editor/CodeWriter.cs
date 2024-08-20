900f1c622b51ec5590c6f8d1eb450fe99d88b30f
dgw0103 <dgw0103@naver.com>
2024-08-20 10:52:35 +0900

    Update CodeWriter.cs

--- Editor/CodeWriter.cs
+++ Editor/CodeWriter.cs
@@ -4,59 +4,59 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
-public class CodeWriter
-{
-    private StringBuilder buffer;
-    private int indentLevel;
-    private static readonly int tabIndentLevel = 4;
+//public class CodeWriter
+//{
+//    private StringBuilder buffer;
+//    private int indentLevel;
+//    private static readonly int tabIndentLevel = 4;
 
 
 
-    public CodeWriter()
-    {
-        buffer = new StringBuilder();
-        indentLevel = 0;
-    }
+//    public CodeWriter()
+//    {
+//        buffer = new StringBuilder();
+//        indentLevel = 0;
+//    }
 
 
 
-    public void BeginBlock()
-    {
-        WriteIndent();
-        buffer.Append("{\n");
-        ++indentLevel;
-    }
-    public void EndBlock()
-    {
-        --indentLevel;
-        WriteIndent();
-        buffer.Append("}\n");
-    }
-    public void WriteLine()
-    {
-        buffer.Append('\n');
-    }
-    public void WriteLine(string text)
-    {
-        if (text.All(char.IsWhiteSpace) == false)
-        {
-            WriteIndent();
-            buffer.Append(text);
-        }
-        buffer.Append('\n');
-    }
-    public void Write(string text)
-    {
-        buffer.Append(text);
-    }
-    public void WriteIndent()
-    {
-        for (var i = 0; i < indentLevel; ++i)
-        {
-            for (var n = 0; n < tabIndentLevel; ++n)
-            {
-                buffer.Append(' ');
-            }
-        }
-    }
-}
+//    public void BeginBlock()
+//    {
+//        WriteIndent();
+//        buffer.Append("{\n");
+//        ++indentLevel;
+//    }
+//    public void EndBlock()
+//    {
+//        --indentLevel;
+//        WriteIndent();
+//        buffer.Append("}\n");
+//    }
+//    public void WriteLine()
+//    {
+//        buffer.Append('\n');
+//    }
+//    public void WriteLine(string text)
+//    {
+//        if (text.All(char.IsWhiteSpace) == false)
+//        {
+//            WriteIndent();
+//            buffer.Append(text);
+//        }
+//        buffer.Append('\n');
+//    }
+//    public void Write(string text)
+//    {
+//        buffer.Append(text);
+//    }
+//    public void WriteIndent()
+//    {
+//        for (var i = 0; i < indentLevel; ++i)
+//        {
+//            for (var n = 0; n < tabIndentLevel; ++n)
+//            {
+//                buffer.Append(' ');
+//            }
+//        }
+//    }
+//}
