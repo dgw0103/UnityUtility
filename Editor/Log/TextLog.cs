17964c89e981341f14349d2adab4eddba6cafaae
dgw0103 <dgw0103@naver.com>
2024-12-13 17:28:24 +0900

    Log function

---
+++ Editor/Log/TextLog.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+using UnityEngine.UI;
+
+public class TextLog
+{
+    private static string canvasPrefabGUID = "2f5279a684768ba478de7cee9d331734";
+    private static string textPrefabGUID = "d663526b84ed19a4cb4880ff6da42899";
+
+
+
+    public static void Log(string text)
+    {
+        GameObject canvasPrefab = CanvasPrefab;
+        GameObject canvasInScene = Object.FindObjectsOfType<GameObject>().
+            SingleOrDefault((x) => PrefabUtility.GetCorrespondingObjectFromSource(x) == canvasPrefab);
+
+        if (canvasInScene == null)
+        {
+            canvasInScene = PrefabUtility.InstantiatePrefab(canvasPrefab) as GameObject;
+        }
+
+        CreateText(canvasInScene.transform.GetChild(0).GetChild(0).GetChild(0), text);
+    }
+    private static void CreateText(Transform textRoot, string text)
+    {
+        GameObject textPrefab = AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(textPrefabGUID), typeof(GameObject)) as GameObject;
+        GameObject textObject = PrefabUtility.InstantiatePrefab(textPrefab, textRoot) as GameObject;
+
+        textObject.GetComponent<Text>().text = text;
+    }
+    private static GameObject CanvasPrefab
+    {
+        get
+        {
+            return AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(canvasPrefabGUID), typeof(GameObject)) as GameObject;
+        }
+    }
+}
